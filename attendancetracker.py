0c614e569a0c338ea1771c62531f9f2284473d44
langdonholmes <55119338+langdonholmes@users.noreply.github.com>
2020-10-25 16:33:15 -0700

    aliases.json implemented.  default config file added.

--- attendancetracker.py
+++ attendancetracker.py
@@ -1,28 +1,29 @@
-import os, sys, re, csv, json
+import os, sys, re, csv, json, configparser
 import pandas as pd
 from os import walk
 from fuzzywuzzy import process
 
-# Names of classes
-sections = {
-    '1': "LING 329-02",
-    '2': "LING 329-04",
-    '3': "LING 460-560",
-    '4': "LING 486",
-}
+config = configparser.ConfigParser()
+try:
+    config.read('attendancetracker.conf')
+except IOerror:
+    try:
+        config.read('default.conf')
+    except IOerror:
+        print("You might want to create a configuration file.")
 
-print('Please choose a section by typing a number between 1 and 4:')
-for x in sections:
-    print('({0}) '.format(x) + sections[x])
+#Load sections from config
+print('Please choose a section by typing a number between 1 and 9:')
+for key in config['classlist']:
+    print('({0}) '.format(key) + config['classlist'][key])
 sectionselection = input()
-section = sections[sectionselection]
+section = config['classlist'][sectionselection]
 
-directory = ("C:/Users/Langdon/CSULB/Sarvenaz Hatami - Fall 2020 - Class Attendance/" + section + "/")
-test = ("C:/Users/Langdon/PycharmProjects/AttendanceTracker/venv/" + section)
+directory = (config['localization']['root_dir'] + section + "/")
 
 # Get directory list for "Zoom Reports" folder
 
-month = "September"
+month = "October"
 
 # For the selection section, check .xlsx for a column matching each date
 # If no date found, call the attendance tracker function
@@ -58,20 +59,9 @@ sessions_recorded.sort(key=datesorter)
 print(sessions_recorded)
 print()
 
-# Records attendance to dataframe.
-# Sometimes, students may log out and log back in
-# Currently only checks if ONE of their sessions is >= 30
-# (not the sum of both sessions)
-
-def recorder(match,ind,prettyname,zoomname):
-    time = zoom.at[ind, "Total Duration (Minutes)"]
-    if time >= 30:
-        attendance.at[match.index, date] = 'P'
-        already_present.append(prettyname)
-        print(prettyname, 'attendance recorded')
-    elif prettyname not in already_present:
-        attendance.at[match.index, date] = str(time)
-        print(prettyname, 'only present for {0} minutes'.format(time))
+# Reads in Aliases dictionary file
+with open('aliases.json','r') as read_file:
+        aliases = json.load(read_file)
 
 # Uses fuzzy matching to pair usernames with names on the roster
 # Asks for confirmation if the match ratio is less than 80
@@ -79,17 +69,35 @@ def recorder(match,ind,prettyname,zoomname):
 # Save confirmed matches to aliases.json +
 # Allow manual pairing of poor matches
 # (e.g. "Samsung Galaxy Note 9")
-
 def tracker(section,date):
     zoom = pd.read_csv(directory + month + "/" + date + " - " + section + ".csv")
     already_present = []
 
+
+    # Records attendance to dataframe.
+    # Sometimes, students may log out and log back in
+    # Currently only checks if ONE of their sessions is >= 30
+    # (not the sum of both sessions)
+    def recorder(match,ind,prettyname,zoomname):
+        time = zoom.at[ind, "Total Duration (Minutes)"]
+        if time >= 30:
+            attendance.at[match.index, date] = 'P'
+            already_present.append(prettyname)
+            print(prettyname, 'attendance recorded')
+        elif prettyname not in already_present:
+            attendance.at[match.index, date] = int(time)
+            print(prettyname, 'only present for {0} minutes'.format(time))
+
     for index, i in enumerate(zoom["Name (Original Name)"]):
         fmatch = process.extractOne(i,attendance['Name'])
-        if fmatch[1] > 80:
+        if i in aliases:
+            prettymatch = aliases.get(i)
+            match = attendance[attendance['Name'].str.contains(prettymatch.strip(), na=False, regex=False)]['Name']
+            recorder(match,index,prettymatch,i)
+        elif fmatch[1] > 80:
             match = attendance[attendance['Name'].str.contains(fmatch[0].strip(), na=False, regex=False)]['Name']
-            recorder(match,index,fmatch[0],i)
             print("matched",i,"to",fmatch[0])
+            recorder(match,index,fmatch[0],i)
         elif fmatch[1] > 50:
             print("Would you like to associate",fmatch[0],"with",i,"?")
             confirm = input("Enter y for yes or n for no\n")
@@ -97,21 +105,39 @@ def tracker(section,date):
                 match = attendance[attendance['Name'].str.contains(fmatch[0].strip(), na=False, regex=False)]['Name']
                 recorder(match,index,fmatch[0],i)
                 print("matched",i,"to",fmatch[0])
+                roster_name = fmatch[0].strip()
+                aliases[i] = roster_name
         else:
-            print("no match detected for", i)
-
-
+            print("************************")
+            print("No match detected for", i)
+            print("************************")
+        print()
+    with open('aliases.json', 'w') as f:
+        json.dump(aliases, f)
     print()
 
-# Writes to attendance sheet in xlsx format
-
-    writer = pd.ExcelWriter(directory + section + ".xlsx")
+    # Writes to attendance sheet in xlsx format
+    writer = pd.ExcelWriter(directory + section + ".xlsx", engine='xlsxwriter')
     attendance.to_excel(writer, sheet_name='Sheet1',index=False)
+    workbook = writer.book
     worksheet = writer.sheets['Sheet1']
+    absent = workbook.add_format({'bg_color': '#FFC7CE',
+                               'bold': True})
+    visiting = workbook.add_format({'bg_color': '#ADD8E6',
+                               'bold': True})
+    worksheet.set_column('A:A',32)
+    worksheet.conditional_format('B2:S34',{'type': 'cell',
+                                             'criteria': '=',
+                                             'value': '"A"',
+                                             'format': absent})
+    worksheet.conditional_format('B2:S34',{'type': 'cell',
+                                            'criteria': 'between',
+                                            'minimum': 0.1,
+                                            'maximum': 31,
+                                            'format': visiting})
     writer.save()
 
 # Run attendance tracker for all unrecorded Zoom reports in the month folder
-
 for date in sessions_recorded:
     if date not in attendance.columns:
         print(date)
